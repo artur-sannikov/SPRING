bb6950535fc379b8170a45de5f0095c7aa9a92a6
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-16 16:25:23 +0300

    del: mixedCCA input

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   description = "Nix Flake for SPRING R package";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     SpiecEasi.url = "github:artur-sannikov/SpiecEasi/nix-flakes";
-    mixedCCA.url = "github:artur-sannikov/mixedCCA/nix-flakes";
   };
   outputs =
     {
@@ -12,32 +11,29 @@
       nixpkgs,
       flake-utils,
       SpiecEasi,
-      mixedCCA,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         SpiecEasiPkg = SpiecEasi.packages.${system}.default;
-        mixedCCAPkg = mixedCCA.packages.${system}.default;
         SPRING = pkgs.rPackages.buildRPackage {
           name = "SPRING";
           src = self;
           propagatedBuildInputs =
             builtins.attrValues {
               inherit (pkgs.rPackages)
-                mixedCCA
+                glmnet
                 huge
+                mixedCCA
+                mvtnorm
                 pulsar
                 rootSolve
-                mvtnorm
                 VGAM
-                glmnet
                 ;
             }
             ++ [
               SpiecEasiPkg
-              mixedCCAPkg
             ];
         };
       in
